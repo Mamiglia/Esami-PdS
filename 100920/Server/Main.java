8a84054fa78b107f302c7ff6508b190a88c9b86d
Matteo Migliarini <33762284+Mamiglia@users.noreply.github.com>
2021-01-19 09:48:31 +0100

    added 100920 Server

---
+++ 100920/Server/Main.java
@@ -0,0 +1,18 @@
+package Server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = new ServerSocket(4400);
+        while (true) {
+            Socket cs = ss.accept();
+            Thread t = new Thread(new Server.ClientHandler(cs));
+            t.start();
+        }
+
+    }
+}
