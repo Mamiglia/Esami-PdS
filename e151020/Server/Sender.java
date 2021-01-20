22e6bf9ac163d93da784c9aa3ab3f94700831a83
Matteo Migliarini <33762284+Mamiglia@users.noreply.github.com>
2021-01-20 23:30:02 +0100

    Added ready-to-use Servers jars

--- e151020/Server/Sender.java
+++ e151020/Server/Sender.java
@@ -1,9 +1,7 @@
 package e151020.Server;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.PrintWriter;
+import java.io.*;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -11,7 +9,7 @@ class Sender  implements Runnable {
     // si occupa solamente di inviare al client le coppie di numeri
     private final ArrayList<Integer> indexList = new ArrayList<>();
     private final PrintWriter output;
-    private final File imageX;
+    private Scanner input;
     public boolean interrupted = false;
 
     public Sender(PrintWriter output, String imageX) {
@@ -19,30 +17,23 @@ class Sender  implements Runnable {
         for (int i=0;i<5;i++) {
             indexList.add(i);
         }
-
-        this.imageX = new File("e151020/Server/" + imageX);
+        System.out.println(Paths.get("").toAbsolutePath().toString());
+        try {
+            this.input = new Scanner(this.getClass().getResourceAsStream("/e151020/Server/" + imageX));
+            System.out.println("Image Found!");
+        } catch (NullPointerException e) {
+            System.out.println("File not found");
+            output.println("File Not Found");
+            output.flush();
+        }
 
     }
 
     @Override
     public void run() {
-        if (!imageX.exists()) {
-            System.out.println("File not found");
-            output.println("File Not Found");
-            output.flush();
-            return;
-        }
-        Scanner scan = null;
-        try {
-            scan = new Scanner(imageX);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Image Found!");
-
-
-        while (scan.hasNextLine() && !interrupted) {
-            String s = scan.nextLine();
+        if (input == null) return;
+        while (input.hasNextLine() && !interrupted) {
+            String s = input.nextLine();
             System.out.println("Sent: " + s);
             output.println(s);
             output.flush();
--- e180620/Server/Main.java
+++ e180620/Server/Main.java
@@ -5,9 +5,11 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
+    public static final int PORT = 4400;
 
     public static void main(String[] args) throws IOException {
-        ServerSocket ss = new ServerSocket(4400);
+        System.out.println("Server opened at " + PORT);
+        ServerSocket ss = new ServerSocket(PORT);
         while (true) {
             Socket cs = ss.accept();
             Thread t = new Thread(new ClientHandler(cs));
